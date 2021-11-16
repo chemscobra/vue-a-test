64010364806262cbbb03edc86708abf9143a5182
Gustavo León <gleondiaz@gmail.com>
2021-11-16 19:58:43 -0300

    inventory listing

---
+++ src/components/ItemList.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-row dense>
+    <v-col v-for="item in items" :key="item.id" cols="12" sm="6" md="4">
+      <ItemCard :item="item" />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import ItemCard from './ItemCard.vue';
+export default {
+  name: 'ItemList',
+  components: {
+    ItemCard
+  },
+  props: {
+    items: {
+      type: Array,
+      default: () => []
+    }
+  }
+};
+</script>
