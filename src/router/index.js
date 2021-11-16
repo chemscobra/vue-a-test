143bb8115e51abfdd386f8849bbf7db6c48c9671
Gustavo León <gleondiaz@gmail.com>
2021-11-16 10:05:09 -0300

    added meta info in routes for later route guard

--- src/router/index.js
+++ src/router/index.js
@@ -7,18 +7,57 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import('../views/Home.vue')
+    component: () => import('../views/Home.vue'),
+    meta: {
+      title: 'Home Page - Vue Test App',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'The home page of a Vue.js Test'
+        },
+        {
+          property: 'og:description',
+          content: 'The home page of a Vue.js Test'
+        }
+      ]
+    }
   },
   {
     path: '/items',
     name: 'Item List',
-    component: () => import('../views/ItemList.vue')
+    component: () => import('../views/ItemList.vue'),
+    meta: {
+      title: 'Item List - Vue Test App',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'The Item List Page of a Vue.js Test'
+        },
+        {
+          property: 'og:description',
+          content: 'The Item List Page of a Vue.js Test'
+        }
+      ]
+    }
   },
   {
     path: '/items/:itemId/detail',
     name: 'Item Detail',
     props: true,
-    component: () => import('../views/ItemDetail.vue')
+    component: () => import('../views/ItemDetail.vue'),
+    meta: {
+      title: 'Item Detail - Vue Test App',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'The Item Detail Page of a Vue.js Test'
+        },
+        {
+          property: 'og:description',
+          content: 'The Item Detail Page of a Vue.js Test'
+        }
+      ]
+    }
   }
 ];
 
