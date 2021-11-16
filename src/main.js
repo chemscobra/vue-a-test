123188684ce8a2ea3b2f2258a3f0e1c699dabff6
Gustavo León <gleondiaz@gmail.com>
2021-11-16 11:10:54 -0300

    refactored axios api module/plugin

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
-import axios from './plugins/axios';
+import API from './plugins/axios';
 
-Vue.prototype.$axios = axios;
+Vue.prototype.$API = API;
 
 Vue.config.productionTip = false;
 
--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,9 @@
 import Axios from 'axios';
 
+const baseURL = process.env.VUE_APP_BASE_API_HOST;
+
 const axios = Axios.create({
-  baseURL: process.env.VUE_APP_BASE_API_HOST,
+  baseURL,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
@@ -9,4 +11,30 @@ const axios = Axios.create({
   }
 });
 
-export default axios;
+export default {
+  listCategories(params) {
+    return axios.get('/categories', {
+      params: params
+    });
+  },
+  listItems(params) {
+    return axios.get('/items', {
+      params: params
+    });
+  },
+  getItem(itemId) {
+    return axios.get(`/items/${itemId}`);
+  },
+
+  updateItem(itemId, payload) {
+    return axios.put(`/items/${itemId}`, payload);
+  },
+
+  createItem(payload) {
+    return axios.post('/items', payload);
+  },
+
+  deleteItem(itemId) {
+    return axios.delete(`/items/${itemId}`);
+  }
+};
