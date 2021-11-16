5f26ddd8b76718620ec486a3efcab203aa934514
Gustavo León <gleondiaz@gmail.com>
2021-11-15 21:05:22 -0300

    added global axios config

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
+import Axios from 'axios';
+
+Vue.prototype.$axios = Axios.create({
+  baseURL: process.env.VUE_APP_BASE_API_HOST,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${process.env.VUE_APP_ACCESS_TOKEN}`
+  }
+});
 
 Vue.config.productionTip = false;
 
